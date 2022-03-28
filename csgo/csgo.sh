4fef3a5f2e566e13ed69407a7fb45a13bdbda2b7
MichaelDeets <MichaelDeets@users.noreply.github.com>
2022-03-28 04:46:34 +0000

    Update csgo.sh

--- csgo/csgo.sh
+++ csgo/csgo.sh
@@ -2,15 +2,15 @@
 GAMEROOT=$(cd "${0%/*}" && echo $PWD)
 UNAME=`uname`
 GAMEEXE=csgo_linux64
-ulimit -n 2048
+#ulimit -n 2048
 export LD_LIBRARY_PATH="${GAMEROOT}"/bin:"${GAMEROOT}"/bin/linux64:$LD_LIBRARY_PATH
 export multithread_glsl_compiler=1
 export AMD_TEX_ANISO=0
 export MESA_NO_ERROR=1
 export vblank_mode=0
 
-export DXVK_HUD=full
-export DXVK_CONFIG_FILE=/home/michael/.dxvk/dxvk.conf
+#export DXVK_HUD=full
+#export DXVK_CONFIG_FILE=/home/michael/.dxvk/dxvk.conf
 
 cd "$GAMEROOT"
 STATUS=42
